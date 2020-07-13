a729bc4e97fcb6f71aa45b5e2663a7a1cdf38e73
Josh Liley WM <j.liley@reply.com>
2020-07-13 13:47:29 +0100

    customisation

---
+++ src/components/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Feedback() {
+  return (
+    <div className="Feedback">
+        <div>How do you rate the site?</div>
+            <div>Love</div>
+            <div>Meh</div>
+            <div>Hate</div>
+    </div>
+  );
+}
+
